8ed8d2a1c3b315c237cd2e254de1b2c31091242c
Aphonic <61099329+MissAphonic@users.noreply.github.com>
2021-08-15 02:52:00 -0700

    Add files via upload

---
+++ TTC-PORTPROTON-SAMPLE-TEST-SCRIPT.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo =======APH-TECH======== &&
+echo Initializing.... &&
+echo ===================== &&
+cd ~/Downloads &&
+echo Loading Script... &&
+echo ===================== &&
+echo Downloading Requested File.... &&
+echo ===================== &&
+curl -s -o ~/Downloads/PriceTable.zip 'https://us.tamrieltradecentre.com/download/PriceTable' &&
+echo Making sure I downloaded the right thing...
+cd ~/Downloads &&
+echo ===================== &&
+echo Unziping and Transfering your file... &&
+echo ===================== &&
+unzip -o -q ~/Downloads/PriceTable.zip -d "/home/gaben/PortWINE/PortProton/drive_c/users/owain/My Documents/Elder Scrolls Online/live/AddOns/TamrielTradeCentre" &&
+echo Done &&
+echo ===================== &&
+echo Removing temporary files &&
+echo ===================== &&
+rm -f "./PriceTable.zip" &&
+echo End of Program.... &&
+echo Terminating Process... &&
+echo Goodbye and Thank you for using our program...
