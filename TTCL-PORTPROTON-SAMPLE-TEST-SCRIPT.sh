7ad8712154739566f27ef659c1051b60009d1b9b
Aphonic <61099329+MissAphonic@users.noreply.github.com>
2021-08-15 02:47:02 -0700

    Add files via upload

---
+++ TTCL-PORTPROTON-SAMPLE-TEST-SCRIPT.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+while [ true ]; do
+echo =======APH-TECH======== &&
+echo Starting Loop Sequence.... &&
+echo ===================== &&
+cd ~/Downloads &&
+echo - NOTICE: &&
+echo If you feel like Im eating resources please do TERMINATE me.. &&
+echo ===================== &&
+echo Downloading Requested File.... &&
+echo ===================== &&
+curl -s -o ~/Downloads/PriceTable.zip 'https://us.tamrieltradecentre.com/download/PriceTable' &&
+echo Making sure I downloaded the right thing...
+cd ~/Downloads &&
+echo ===================== &&
+echo Unziping and Transfering your file... &&
+echo ===================== &&
+unzip -o -q ~/Downloads/PriceTable.zip -d "/home/gaben/PortWINE/PortProton/drive_c/users/owain/My Documents/Elder Scrolls Online/live/AddOns/TamrielTradeCentre" &&
+echo Done &&
+echo ===================== &&
+echo Removing temporary files &&
+echo ===================== &&
+rm -f "./PriceTable.zip" &&
+echo Restarting Loop Sequence.. &&
+echo ===================== &&
+done
